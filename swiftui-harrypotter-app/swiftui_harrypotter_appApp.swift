8a7a2a55d253b31b5a4913488daa37fc4416378f
anaygupta <anay0@icloud.com>
2022-05-29 12:33:25 -0700

    Initial Commit

---
+++ swiftui-harrypotter-app/swiftui_harrypotter_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_harrypotter_appApp.swift
+//  swiftui-harrypotter-app
+//
+//  Created by Anay on 5/29/22.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_harrypotter_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swiftui-harrypotter-appUITests/swiftui_harrypotter_appUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  swiftui_harrypotter_appUITestsLaunchTests.swift
+//  swiftui-harrypotter-appUITests
+//
+//  Created by Anay on 5/29/22.
+//
+
+import XCTest
+
+class swiftui_harrypotter_appUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
